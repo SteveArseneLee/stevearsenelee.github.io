ea3bf5a24694f7e8f24eb70631edac2659f35dd0
SteveArseneLee <lclgood@naver.com>
2021-07-03 01:01:47 +0900

    bla

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅하는 방법",
+        "excerpt":"Minimal Mistakes Jekyll theme Minimal Mistakes is a flexible two-column Jekyll theme, perfect for building personal sites, blogs, and portfolios. As the name implies, styling is purposely minimalistic to be enhanced and customized by you :smile:. :sparkles: See what’s new in the CHANGELOG. If you enjoy this theme, please consider...","categories": ["Test"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/test/my-first-post/",
+        "teaser": null
+      }]
